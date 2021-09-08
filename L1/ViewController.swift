23a85c9dcae44abd919133afce49fe54ec81f487
Aleksandrs Sivickis <biggy.man747@gmail.com>
2021-09-08 04:22:57 +0300

    First commit

---
+++ L1/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  L1
+//
+//  Created by Aleksandrs Sivickis on 06/09/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet private weak var loginField: UITextField!
+    
+    @IBOutlet private weak var passwordField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction private func enterPressed(_ sender: Any) {
+        let loginName = loginField.text
+        let password = passwordField.text
+        
+        if loginName == "admin" && password == "123" {
+            print("Authorized")
+            
+        }
+    }
+    
+}
+
