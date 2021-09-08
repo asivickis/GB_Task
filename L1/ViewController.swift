771fc23716187272e517117e16a12e713136e7f2
Aleksandrs Sivickis <biggy.man747@gmail.com>
2021-09-08 04:40:11 +0300

    removed comment

--- L1/ViewController.swift
+++ L1/ViewController.swift
@@ -15,7 +15,6 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
     @IBAction private func enterPressed(_ sender: Any) {
