4682a41452b7d68a11e1cd6f082cb02a5c70fc68
Aleksandrs Sivickis <biggy.man747@gmail.com>
2021-09-10 03:49:01 +0300

    added error + admin board

---
+++ L1/AdminBoard/AdminViewController.swift
@@ -0,0 +1,14 @@
+//
+//  AdminViewController.swift
+//  L1
+//
+//  Created by Aleksandrs Sivickis on 10/09/2021.
+//
+
+import UIKit
+
+class AdminViewController:  UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
