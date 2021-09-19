e00bea8baf62b627bef01f160a5a98af6b6e9ddc
Aleksandrs Sivickis <biggy.man747@gmail.com>
2021-09-19 07:10:58 +0300

    added test draw boards + applied star under admin board

--- L1/AdminBoard/AdminViewController.swift
+++ L1/AdminBoard/AdminViewController.swift
@@ -35,3 +35,29 @@ class subAdminViewController: UIViewController {
         self.showContactBoard()
     }
 }
+
+
+class logo: UIView {
+
+    override func draw(_ rect: CGRect) {
+        super.draw(rect)
+        guard let context = UIGraphicsGetCurrentContext() else { return }
+        context.setStrokeColor(UIColor.red.cgColor)
+        let starPath = UIBezierPath()
+        starPath.move(to: CGPoint(x: 200, y: 120))
+        starPath.addLine(to: CGPoint(x: 130, y: 240))
+        starPath.addLine(to: CGPoint(x: 30, y: 240))
+        starPath.addLine(to: CGPoint(x: 100, y: 300))
+        starPath.addLine(to: CGPoint(x: 50, y: 400))
+        starPath.addLine(to: CGPoint(x: 200, y: 320))
+        starPath.addLine(to: CGPoint(x: 350, y: 400))
+        starPath.addLine(to: CGPoint(x: 300, y: 300))
+        starPath.addLine(to: CGPoint(x: 370, y: 240))
+        starPath.addLine(to: CGPoint(x: 270, y: 240))
+        starPath.close()
+        starPath.stroke()
+ 
+    }
+
+
+}
